6cbec9f5b9655ac185548e97935354545c289b54
Tristan AI-Guru <tristan@ai-guru.de>
2018-08-14 13:23:51 +0200

    Enables floodfill.

--- mandalapainter.py
+++ mandalapainter.py
@@ -1,6 +1,6 @@
 from tkinter import *
 from tkinter import filedialog
-from PIL import Image, ImageTk, ImageDraw, ImageFilter, ImageChops, ImageEnhance
+from PIL import Image, ImageTk, ImageDraw, ImageFilter, ImageChops, ImageEnhance, ImageColor
 from tkinter.colorchooser import askcolor
 import math
 import numpy as np
@@ -25,8 +25,10 @@ class Paint(object):
         self.color_button.grid(row=1, column=column)
         column += 1
 
-        self.segments_button = Button(self.root, text='Segments', command=self.draw_segments)
-        self.segments_button.grid(row=0, column=column)
+        self.draw_button = Button(self.root, text='Draw', command=self.enable_draw)
+        self.draw_button.grid(row=0, column=column)
+        self.fill_button = Button(self.root, text='Fill', command=self.enable_fill)
+        self.fill_button.grid(row=1, column=column)
         column += 1
 
         self.choose_size_button = Scale(self.root, from_=1, to=10, orient=HORIZONTAL, label="Pen Size")
@@ -53,6 +55,7 @@ class Paint(object):
         self.segment_vectors = []
 
         self.mirror = False
+        self.mode = "draw"
 
         self.c = Canvas(self.root, bg='red', width=self.size[0], height=self.size[0])
         self.c.grid(row=2, columnspan=column)
@@ -81,6 +84,15 @@ class Paint(object):
     def toggle_mirror(self):
         self.mirror = not self.mirror
 
+
+    def enable_draw(self):
+        self.mode = "draw"
+
+
+    def enable_fill(self):
+        self.mode = "fill"
+
+
     def draw_segments(self):
         for segment in range(self.segments):
             pass
@@ -97,14 +109,17 @@ class Paint(object):
         self.eraser_on = False
         self.color = askcolor(color=self.color)[1]
 
-    def use_eraser(self):
-        self.activate_button(self.eraser_button, eraser_mode=True)
 
-    def activate_button(self, some_button, eraser_mode=False):
-        self.active_button.config(relief=RAISED)
-        some_button.config(relief=SUNKEN)
-        self.active_button = some_button
-        self.eraser_on = eraser_mode
+#    def use_eraser(self):
+#        self.activate_button(self.eraser_button, eraser_mode=True)
+
+
+#    def activate_button(self, some_button, eraser_mode=False):
+#        self.active_button.config(relief=RAISED)
+#        some_button.config(relief=SUNKEN)
+#        self.active_button = some_button
+#        self.eraser_on = eraser_mode
+
 
     def paint(self, event):
 
@@ -119,25 +134,26 @@ class Paint(object):
             # For debugging.
             #self.render_segments(self.clicked_segment)
 
-        self.line_width = self.choose_size_button.get()
-        self.segments = self.choose_segments.get()
-        self.blur_degree = self.choose_blur.get() / 10.0
+        if self.mode == "draw":
+            self.line_width = self.choose_size_button.get()
+            self.segments = self.choose_segments.get()
+            self.blur_degree = self.choose_blur.get() / 10.0
 
-        brush_offsets = [(-5, 2), (2, 12), (-20, 2), (8, -4), (-4, -6)]
+            brush_offsets = [(-5, 2), (2, 12), (-20, 2), (8, -4), (-4, -6)]
 
-        if self.old_x and self.old_y:
-            distance = get_distance(self.old_x, self.old_y, event.x, event.y)
-            if distance > 1.0:
-                self.draw_line(self.old_x, self.old_y, event.x, event.y)
+            if self.old_x and self.old_y:
+                distance = get_distance(self.old_x, self.old_y, event.x, event.y)
+                if distance > 1.0:
+                    self.draw_line(self.old_x, self.old_y, event.x, event.y)
 
-                #for (x, y) in brush_offsets:
-                #    self.draw_line(x + self.old_x, y + self.old_y, x + event.x, y + event.y)
+                    #for (x, y) in brush_offsets:
+                    #    self.draw_line(x + self.old_x, y + self.old_y, x + event.x, y + event.y)
 
+                    self.old_x = event.x
+                    self.old_y = event.y
+            else:
                 self.old_x = event.x
                 self.old_y = event.y
-        else:
-            self.old_x = event.x
-            self.old_y = event.y
 
 
     def render_segments(self, clicked_segment):
@@ -223,8 +239,19 @@ class Paint(object):
 
 
     def reset(self, event):
-        self.old_x, self.old_y = None, None
-        self.clicked_segment = None
+        del self.imagesprite
+        del self.image
+
+        if self.mode == "draw":
+            self.old_x, self.old_y = None, None
+            self.clicked_segment = None
+
+        elif self.mode == "fill":
+            ImageDraw.floodfill(self.pilImage, xy=(event.x, event.y), value=ImageColor.getrgb(self.color))
+
+        self.image = ImageTk.PhotoImage(self.pilImage)
+        self.imagesprite = self.c.create_image(self.center[0], self.center[1], image=self.image)
+
 
 
 def vector_from_angle_and_length(angle, length):
