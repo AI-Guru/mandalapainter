27ed8116db30608e157d5bcc4b7569a64dc4c121
Tristan AI-Guru <tristan@ai-guru.de>
2018-08-18 20:06:47 +0200

    progress.

--- mainwindow.py
+++ mainwindow.py
@@ -1,6 +1,7 @@
 import tkinter as tk
 from toolswindow import *
 from toolprocessors import *
+from tkinter.colorchooser import askcolor
 from PIL import Image, ImageTk, ImageDraw, ImageFilter, ImageChops, ImageEnhance, ImageColor, ImageOps
 
 class MainWindow(tk.Frame):
@@ -10,11 +11,6 @@ class MainWindow(tk.Frame):
         # TODO set title
 
         column = 0
-
-        # Adds a button for mirroring.
-        self.mirror_button = tk.Button(self, text="Mirror", command=self.click)
-        self.mirror_button.grid(row=0, column=column)
-        column += 1
 
         # Save button.
         self.save_button = tk.Button(self, text="Save", command=self.click)
@@ -26,14 +22,34 @@ class MainWindow(tk.Frame):
         self.undo_button.grid(row=0, column=column)
         column += 1
 
+        # Color.
+        self.color_button = tk.Button(self, text='Color', command=self.color_button_clicked)
+        self.color_button.grid(row=0, column=column)
+        column += 1
+
+        # Selecting the number of segments.
+        self.label = tk.Label(self, text="Segments:")
+        self.label.grid(row=0, column=column)
+        column += 1
+        self.segments_spinbox = tk.Spinbox(self, width=3, from_=4, to=100, command=self.segments_spinbox_changed)
+        self.segments_spinbox.grid(row=0, column=column)
+        self.segments_spinbox.delete(0,"end")
+        self.segments_spinbox.insert(0 ,32)
+        column += 1
+
+        # Mirror button.
+        self.mirror_checkbutton_var = tk.IntVar()
+        self.mirror_checkbutton = tk.Checkbutton(self, text="Mirror", variable=self.mirror_checkbutton_var, command=self.mirror_checkbutton_changed)
+        self.mirror_checkbutton.grid(row=0, column=column)
+
         self.width = 800
         self.height = 800
         self.center_x = self.width // 2
         self.center_y = self.height // 2
-        self.segments = 32
-        self.mirror = True
+        #self.segments = 32
+        #self.mirror = True
         self.color = "white"
-        self.line_width = 2
+        self.line_width = 20
 
         # Create the canvas.
         self.canvas = tk.Canvas(self, bg="red", width=self.width, height=self.height)
@@ -56,9 +72,24 @@ class MainWindow(tk.Frame):
         # Create tools-window.
         self.tools_window = ToolsWindow(self)
 
+        self.segments_spinbox_changed()
+        self.mirror_checkbutton_changed()
+
 
     def click(self):
         pass
+
+
+    def color_button_clicked(self):
+        self.color = askcolor(color=self.color)[1]
+
+
+    def segments_spinbox_changed(self):
+        self.segments = int(self.segments_spinbox.get())
+
+
+    def mirror_checkbutton_changed(self):
+        self.mirror = self.mirror_checkbutton_var.get() == 1
 
 
     def notify_tool_change(self, new_tool):
